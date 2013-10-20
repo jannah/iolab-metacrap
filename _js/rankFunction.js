0426ffaf3c3cb592719d8d262f07b986c123ab58
christopherfan <chrisfan@outlook.com>
2013-10-20 14:12:23 -0700

    Adding Search Histogram Code

--- _js/rankFunction.js
+++ _js/rankFunction.js
@@ -42,83 +42,98 @@ function rankFunction() {
 
     //
     $('#tweet-tag-suggest').click(function () {
-        var word_list = createKeys();
+        var word_list = createKeys($('#tweet-text').val());
         word_list.done(function (data) {
-            console.log(data);
+            var master_histogram = {};
+            generateSearchHistogram(data, master_histogram).done( function(){
+                console.log("Master Histogram Completed");
+                console.log(master_histogram);
+                sortHistogram(master_histogram);
+            });
+
         });
     });
 
     
 
 
-    var histogram_universe = {};
-    var histogram_local = {};
+    //var histogram_universe = {};
+    //var histogram_local = {};
 
-    deferredObject = callTwitterAPI(parameters,api);
-    deferredObject.done(function (data) {
-        console.log("Equivalent Search API using generic API CALL");
-        console.log(data);
-        tweets = convertToTweets(data[0]);        
-        console.log("Local Results &&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
-        $.each(tweets, function (index, value) {
+    //deferredObject = callTwitterAPI(parameters,api);
+    //deferredObject.done(function (data) {
+    //    console.log("Equivalent Search API using generic API CALL");
+    //    console.log(data);
+    //    tweets = convertToTweets(data[0]);        
+    //    console.log("Local Results &&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+    //    $.each(tweets, function (index, value) {
 
-            if (value.hashtags.length > 0) {
-                //console.log(value.hashtags);
-                for (var counter = 0; counter < value.hashtags.length; counter++) {
-                    addToDictionary(histogram_local, value.hashtags[counter]);
-                }
-            }
-        });
-        console.log("Local: " , histogram_local);
-        console.log("\n");
-    });
+    //        if (value.hashtags.length > 0) {
+    //            //console.log(value.hashtags);
+    //            for (var counter = 0; counter < value.hashtags.length; counter++) {
+    //                addToDictionary(histogram_local, value.hashtags[counter]);
+    //            }
+    //        }
+    //    });
+    //    console.log("Local: " , histogram_local);
+    //    console.log("\n");
+    //});
 
     //get results from a Twitter Query without geolocation    
-    deferredObject = getSearchResults(query, count);
-    deferredObject.done(function (data) {
-        console.log("Universe Search Query Data");
-
-        console.log("Equivalent Search API using generic API CALL");
-        console.log(data);
-        tweets = convertToTweets(data[0]);
-        console.log("Universal Results @@@@@@@@@@@@@@@@@@@@@");
-        $.each(tweets, function (index, value) {
-
-            if (value.hashtags.length > 0) {
-               // console.log(value.hashtags);
-                for (var counter = 0; counter < value.hashtags.length; counter++) {
-                    addToDictionary(histogram_universe, value.hashtags[counter]);
-                }
-            }
-        });
-        console.log("Universe: ", histogram_universe);
-        console.log("\n");
-
-        print_histogram("#histogram_list", histogram_universe);
-    });
 
 
-
-    // //post Tweet
-    // //return error if you try to tweet the same tweet text twice 
-    //var tweet = 'Go Bears!';
-    //var deferredObject = postTweet(tweet);
-    //deferredObject.done(function (data) {
-    //    console.log("Post Data");
-    //    console.log(data);
-    //});
+    
+    $('#histogram').append( "<li> something here </li>");
+ 
 	
     
 }
 
+function getHashTagsFromSearch(word, master_histogram) {
+
+    return getSearchResults(word, 100).pipe(function (data) {        
+        var tweets = convertToTweets(data[0]);
+        $.each(tweets, function (index, value) {
+            if (value.hashtags.length > 0) {
+                // console.log(value.hashtags);
+                for (var counter = 0; counter < value.hashtags.length; counter++) {
+                    addToDictionary(master_histogram, value.hashtags[counter]);
+                }
+            }
+        });
+        console.log("Histogram for ", word, " size ", sizeofObject(master_histogram));
+        console.log(master_histogram, "\n");
+        return master_histogram;
+    });
+}
+
+function generateSearchHistogram(text_list, master_histogram) {
+    var histogram = {};
+    var api_call_list = $.map(text_list, function (query) {
+        return getHashTagsFromSearch(query, master_histogram);
+    });
+
+    return $.when.apply($, api_call_list);
+}
+
+
 function print_histogram(dom_location, histogram) {
-    var text= "<li> ";
+    
+    var text = "<li> ";
     $.each(histogram, function (key, value) {
         text += "[" + key + ":" + value + "]" + "  ";
     });
     text += " </li>";
-    $(dom_location).append(text);
+    $('#histogram_list').append("<li> something here </li>");
 
+}
+
+function sizeofObject(object) {
+    var size=0;
+    $.each(object, function () {
+        size++;
+    });
+    return size;
 }
 
 function addToDictionary(dictionary, element) {
@@ -131,4 +146,32 @@ function addToDictionary(dictionary, element) {
         dictionary[element] = 1;
     }
 
+}
+
+function sortHistogram(histogram) {
+    var sorted = [{ 'number': 1, hashtags: [] }];
+    $.each(histogram, function (key, value) {
+       // console.log("searching,", key, " ", value);
+        var result = $.grep(sorted, function (item) { return item.number == value });
+        //console.log("grep result ", result);
+        if (result.length == 0) {
+            strings =[];
+            strings.push(key);
+           // console.log("pushing: ", value, " and", strings);
+            sorted.push({ 'number': value, 'hashtags': strings });
+        } else if (result.length == 1) {
+         //   console.log(" found it");
+            result[0].hashtags.push(key);
+        }
+    });
+    var temp =sorted.sort(function(a,b){
+        if (a.number > b.number)
+            return -1;
+        if (a.number < b.number)
+            return 1;
+        // a must be equal to b
+        return 0;
+    });
+    console.log(temp);
+    return sorted;
 }
